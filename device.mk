0bdc5adeee396f6e430e95f10d45ce50c5d5ce1d
Mod4 <omarelfarok24135@gmail.com>
2026-03-27 11:43:02 +0400

    Update device.mk

--- device.mk
+++ device.mk
@@ -6,3 +6,28 @@
 #
 
 LOCAL_PATH := device/samsung/a23
+
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# fastbootd
+TW_INCLUDE_FASTBOOTD := true
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+# qcom decryption
+@@ -27,7 +29,8 @@ PRODUCT_PACKAGES += \
+
+# Dependencies
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libion
+    libion \
+    libfastboot
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
